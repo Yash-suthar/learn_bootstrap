e0ffd2660eb11f727a65d8d0f114e07512ea7769
yash-suthar <yashsuthar529@gmail.com>
2022-05-23 19:03:11 +0530

    change in index.html file add image

--- day2/index.js
+++ day2/index.js
@@ -2,7 +2,7 @@
 var current_time = document.getElementById("time");
 setInterval(()=>{
     var time = new Date();
-    current_time.innerHTML= `${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}`;
+    current_time.innerHTML= `${time.getHours()%12}:${time.getMinutes()}:${time.getSeconds()}`;
     // current_time.innerText += "hello";
 },1000);
 
