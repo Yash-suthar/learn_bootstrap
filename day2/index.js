9b0e53f16b632be3246e785c576521be2726d918
yash-suthar <yashsuthar529@gmail.com>
2022-05-23 15:05:56 +0530

    learning date function and interval function in javascript

---
+++ day2/index.js
@@ -0,0 +1,9 @@
+
+var current_time = document.getElementById("time");
+setInterval(()=>{
+    var time = new Date();
+    current_time.innerHTML= `${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}`;
+    // current_time.innerText += "hello";
+},1000);
+
+
